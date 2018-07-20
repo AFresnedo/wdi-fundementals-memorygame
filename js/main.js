caf97e1730ed27aceec5bc498cfe8b8e758bdb4c
Andres Fresnedo <andfresnedo@gmail.com>
2018-07-19 20:12:27 -0700

    tested card names

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+var cardOne;
+var cardTwo;
+var cardThree;
+var cardFour;
+cardOne = "king";
+cardTwo = "king";
+cardThree = "queen";
+cardFour = "queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
