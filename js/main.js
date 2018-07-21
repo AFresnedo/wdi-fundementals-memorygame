58b0dd85c36afacca1e67e9a281b30cb3228314e
Andres Fresnedo <andfresnedo@gmail.com>
2018-07-21 04:25:55 -0700

    refactor game logic into functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
+// card items
 var cards, cardsInPlay;
 var cardOne, cardTwo;
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-cardOne = cards[2];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0])
-cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1])
-if (cardsInPlay.length === 2) {
+
+// game logic
+
+// function to determine if the flipped cards are a match
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 	else {
-		alert("Try again!");
+		alert("You don't have a match, yet.");
 	}
 }
+
+// precondition: card unflipped
+var flipCard = function (cardId) {
+	// get card to play
+	var flippedCard = cards[cardId];
+	// add card to "in play"
+	cardsInPlay.push(flippedCard);
+	// give feedback to user
+	console.log("User flipped " + flippedCard);
+	checkForMatch();
+}
+
+// simulated user actions
+flipCard(0);
+flipCard(1);
